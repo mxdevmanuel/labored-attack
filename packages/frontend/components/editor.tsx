cecc3faaa5c2aefc2869f24bfa4cb99dcf29eb4b
Manuel Morales <p513manu@gmail.com>
2021-12-06 09:22:44 -0600

    just trolling the repo

--- packages/frontend/components/editor.tsx
+++ packages/frontend/components/editor.tsx
@@ -4,6 +4,7 @@ import { languages } from '@data/constants';
 import {
   UIEvent,
   FormEvent,
+  KeyboardEvent,
   ChangeEvent,
   useState,
   useEffect,
@@ -22,17 +23,26 @@ const selectStyle =
 const editor =
   'absolute top-0 bottom-0 min-w-full p-2 rounded-lg overflow-auto focus:outline-none';
 
-export default function Editor() {
+interface EditorProps {
+  title?: string;
+  onCancel?: () => void;
+}
+
+export default function Editor(
+  props: EditorProps = { title: 'Create new snippet' },
+) {
   const preRef = useRef<HTMLPreElement>();
   const [language, setLanguage] = useState('javascript');
   const [code, setCode] = useState('');
   const [highlightedCode, setHighlightedCode] = useState('');
+
   useEffect(() => {
     setHighlightedCode(hljs.highlight(code, { language }).value);
   }, [code, language]);
+
   return (
     <div className="flex flex-col place-content-center p-4 mx-auto w-5/6 lg:w-2/3 2xl:w-1/4 font-publicsans">
-      <h2 className="text-4xl text-orange-400">Create new snippet</h2>
+      <h2 className="text-4xl text-orange-400">{props.title}</h2>
       <input type="text" placeholder="// Title" className={clsx(inputStyle)} />
       <div className="relative h-72 bg-white border-4 rounded-lg border-sky-700">
         <textarea
@@ -42,6 +52,12 @@ export default function Editor() {
             'text-transparent ring-transparent bg-transparent caret-sky-900 z-50 focus:outline-none resize-none',
             editor,
           )}
+          onKeyDown={(e: KeyboardEvent<HTMLTextAreaElement>) => {
+            if (e.key === "Tab") {
+              e.preventDefault()
+              e.currentTarget.setRangeText('  ', e.currentTarget.selectionStart, e.currentTarget.selectionStart, 'end')
+            }
+          }}
           onScroll={(e: UIEvent<HTMLTextAreaElement>) => {
             preRef.current.scrollTop = e.currentTarget.scrollTop;
             preRef.current.scrollLeft = e.currentTarget.scrollLeft;
@@ -51,6 +67,9 @@ export default function Editor() {
             preRef.current.scrollLeft = e.currentTarget.scrollLeft;
             let preproc: string = e.currentTarget.value;
             if (preproc.endsWith('\n')) {
+              preproc += ' ';
+            }
+            if (preproc.endsWith('\t')) {
               preproc += ' ';
             }
             setCode(preproc);
@@ -66,7 +85,7 @@ export default function Editor() {
           <code dangerouslySetInnerHTML={{ __html: highlightedCode }}></code>
         </pre>
       </div>
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row justify-between my-2">
         <select
           id="something"
           placeholder="Language"
@@ -85,10 +104,17 @@ export default function Editor() {
             </option>
           ))}
         </select>
-        <button className="rounded-lg px-5 py-2 bg-orange-400 text-white text-xl font-semibold">
-          Submit
+        <button className="rounded-lg px-8 py-2 bg-orange-400 text-white text-xl font-semibold">
+          Save
         </button>
       </div>
+      {!isNil(props.onCancel) && (
+        <div className="flex flex-row justify-end my-1">
+          <span className="text-red-500 underline hover:text-red-300 cursor-pointer">
+            Cancel
+          </span>
+        </div>
+      )}
     </div>
   );
 }
